44a15ad8c07f1987031e05883cc7ad163a634dee
jir0175 <19aleksandrtarasov@gmail.com>
2025-12-15 22:34:34 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = process.env.BOT_TOKEN;
+
+const app = express();
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+let CHAT_ID = null;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+bot.on('message', (msg) => {
+  if (!CHAT_ID) {
+    CHAT_ID = msg.chat.id;
+    bot.sendMessage(CHAT_ID, '✅ Чат подключён. Сайт готов принимать данные.');
+    console.log('Chat ID:', CHAT_ID);
+  }
+});
+
+app.post('/send', (req, res) => {
+  const number = req.body.number;
+
+  if (!CHAT_ID) {
+    return res.send('❌ Сначала напишите боту в Telegram');
+  }
+
+  bot.sendMessage(CHAT_ID, `📩 С сайта пришло число: ${number}`);
+  res.send('✅ Отправлено в Telegram');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server started'));
